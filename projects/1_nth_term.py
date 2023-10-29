1fe633944afd1f81c2e7b3f03750e9731b60ff92
Avishek Sen <x0rzavi@gmail.com>
2023-10-29 23:23:33 +0530

    project: add

---
+++ projects/1_nth_term.py
@@ -0,0 +1,9 @@
+# series: 1,1,2,3,4,9,8,27,16,81,32,243,64,729,128,2187...
+def nth_term(n):
+    if n % 2 == 0:
+        return 3 ** ((n // 2) - 1)
+    else:
+        return 2 ** (n // 2)
+
+n = int(input('Enter n: '))
+print(nth_term(n))
